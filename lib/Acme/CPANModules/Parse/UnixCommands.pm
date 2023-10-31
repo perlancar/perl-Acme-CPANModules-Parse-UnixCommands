f0c8d1909985e6c110419a81dc7755e39e6c09bd
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-10-31 12:20:40 +0700

    Tweak Abstract (add 'List of ...' prefix) to follow convention

--- lib/Acme/CPANModules/Parse/UnixCommands.pm
+++ lib/Acme/CPANModules/Parse/UnixCommands.pm
@@ -9,7 +9,7 @@ use warnings;
 # VERSION
 
 our $LIST = {
-    summary => "Modules that parse output of Unix commands",
+    summary => "List of modules that parse output of Unix commands",
     entries => [
         {module=>'Cisco::ShowIPRoute::Parser', summary=>'Parse Cisco "show ip route" command'},
         {module=>'IPTables::Parse', summary=>'Parse iptables output'},
